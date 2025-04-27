817672a01234819af43878f211b3d6068e50584b
Jetaofgak <dahamitaoufik@gmail.com>
2025-04-27 16:22:14 +0100

    Force Push Overhaul of the app

--- app/src/main/java/com/example/schoolapp/AddSubject.java
+++ app/src/main/java/com/example/schoolapp/AddSubject.java
@@ -1,6 +1,5 @@
 package com.example.schoolapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,14 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class AddSubject extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_add_subject);
+
     }
 }
